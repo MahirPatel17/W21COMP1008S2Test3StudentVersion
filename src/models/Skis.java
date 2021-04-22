c313a97264cc2333c9524f40fef008f1e0ded840
MahirPatel17 <78319863+MahirPatel17@users.noreply.github.com>
2021-04-22 12:45:08 -0400

    Question 1 completed

---
+++ src/models/Skis.java
@@ -0,0 +1,35 @@
+// Name - Mahir Patel
+// Student number - 200454762
+
+package models;
+
+import Utilities.DBUtility;
+
+public class Skis {
+    private String brand, model;
+    private int length;
+
+    public Skis(String brand, String model, int length,double purchasePrice, double sellingPrice, int quantityInStock) {
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+    }
+}
