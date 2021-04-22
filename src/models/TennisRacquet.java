878183e6429834b7523bf9a109fad0f7533d29c9
MahirPatel17 <78319863+MahirPatel17@users.noreply.github.com>
2021-04-22 13:11:28 -0400

    Question 2 Completed

--- src/models/TennisRacquet.java
+++ src/models/TennisRacquet.java
@@ -5,20 +5,34 @@ package models;
 
 import Utilities.DBUtility;
 
-public class TennisRacquet  {
+public class TennisRacquet extends InventoryItem  {
     private double weight;  //240-310
     private String headSize; //midsize, midplus, oversize, super oversize
     private String brand; //Head, Babolat, Dunlop, Yonex
     private String model;
 
     public TennisRacquet(String brand, String model, double weight, String headSize, double purchasePrice, double sellingPrice, int quantityInStock) {
+        super(purchasePrice, sellingPrice, quantityInStock);
+        setWeight(weight);
+        setHeadSize(headSize);
+        setBrand(brand);
+        setModel(model);
     }
+
 
     public double getWeight() {
         return weight;
     }
 
     public void setWeight(double weight) {
+        if(weight >= 240 && weight <= 310)
+        {
+            this.weight = weight;
+        }
+        else
+        {
+            throw new IllegalArgumentException("weight must be in range of 240 - 310");
+        }
     }
 
     public String getHeadSize() {
